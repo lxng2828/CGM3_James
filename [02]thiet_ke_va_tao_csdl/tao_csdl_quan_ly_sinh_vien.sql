6b79b56b736ad53260d29c099a125b6d8b6089db
VietLong <vietlong81k@gmail.com>
2025-05-21 10:08:16 +0700

    .

--- [02]thiet_ke_va_tao_csdl/tao_csdl_quan_ly_sinh_vien.sql
+++ [02]thiet_ke_va_tao_csdl/tao_csdl_quan_ly_sinh_vien.sql
@@ -1,37 +1,37 @@
 CREATE DATABASE quan_ly_sinh_vien;
 USE quan_ly_sinh_vien;
 
-CREATE TABLE class (
-    class_id    INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    class_name  VARCHAR(60) NOT NULL,
-    start_date  DATETIME    NOT NULL,
-    status      BIT
+CREATE TABLE lop_hoc (
+    lop_hoc_id   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ten_lop      VARCHAR(60) NOT NULL,
+    ngay_bat_dau DATETIME    NOT NULL,
+    trang_thai   BIT
 );
 
-CREATE TABLE student (
-    student_id   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    student_name VARCHAR(30) NOT NULL,
-    address      VARCHAR(50),
-    phone        VARCHAR(20),
-    status       BIT,
-    class_id     INT         NOT NULL,
-    FOREIGN KEY (class_id) REFERENCES class (class_id)
+CREATE TABLE sinh_vien (
+    sinh_vien_id   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ho_ten         VARCHAR(30) NOT NULL,
+    dia_chi        VARCHAR(50),
+    so_dien_thoai  VARCHAR(20),
+    trang_thai     BIT,
+    lop_hoc_id     INT         NOT NULL,
+    FOREIGN KEY (lop_hoc_id) REFERENCES lop_hoc (lop_hoc_id)
 );
 
-CREATE TABLE subject (
-    sub_id    INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    sub_name  VARCHAR(30) NOT NULL,
-    credit    TINYINT     NOT NULL DEFAULT 1 CHECK (credit >= 1),
-    status    BIT                  DEFAULT 1
+CREATE TABLE mon_hoc (
+    mon_hoc_id INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ten_mon    VARCHAR(30) NOT NULL,
+    so_tin_chi TINYINT     NOT NULL DEFAULT 1 CHECK (so_tin_chi >= 1),
+    trang_thai BIT                  DEFAULT 1
 );
 
-CREATE TABLE mark (
-    mark_id     INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    sub_id      INT NOT NULL,
-    student_id  INT NOT NULL,
-    mark        FLOAT   DEFAULT 0 CHECK (mark BETWEEN 0 AND 100),
-    exam_times  TINYINT DEFAULT 1,
-    UNIQUE (sub_id, student_id),
-    FOREIGN KEY (sub_id) REFERENCES subject (sub_id),
-    FOREIGN KEY (student_id) REFERENCES student (student_id)
+CREATE TABLE diem_thi (
+    diem_thi_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    mon_hoc_id  INT NOT NULL,
+    sinh_vien_id INT NOT NULL,
+    diem        FLOAT   DEFAULT 0 CHECK (diem BETWEEN 0 AND 100),
+    lan_thi     TINYINT DEFAULT 1,
+    UNIQUE (mon_hoc_id, sinh_vien_id),
+    FOREIGN KEY (mon_hoc_id) REFERENCES mon_hoc (mon_hoc_id),
+    FOREIGN KEY (sinh_vien_id) REFERENCES sinh_vien (sinh_vien_id)
 );
