d8913010f8d64edefce14c62f309a800b7bab82d
lxng <lxng@lxng>
2025-05-21 14:04:37 +0700

    .

--- [02]thiet_ke_va_tao_csdl/xay_dung_co_so_du_lieu_quan_ly_ban_hang.sql
+++ [02]thiet_ke_va_tao_csdl/xay_dung_co_so_du_lieu_quan_ly_ban_hang.sql
@@ -1,32 +1,32 @@
-CREATE DATABASE QuanLyBanHang;
+CREATE DATABASE quan_ly_ban_hang;
 
-USE QuanLyBanHang;
+USE quan_ly_ban_hang;
 
-CREATE TABLE Customer (
-    cID INT PRIMARY KEY AUTO_INCREMENT,
-    cName VARCHAR(255) NOT NULL,
-    cAge INT
+CREATE TABLE khach_hang (
+    khach_hang_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    ten_khach_hang VARCHAR(100) NOT NULL,
+    tuoi_khach_hang TINYINT UNSIGNED CHECK (tuoi_khach_hang >= 0 AND tuoi_khach_hang <= 120)
 );
 
-CREATE TABLE Product (
-    pID INT PRIMARY KEY AUTO_INCREMENT,
-    pName VARCHAR(255) NOT NULL UNIQUE,
-    pPrice DECIMAL(10, 2) NOT NULL CHECK (pPrice > 0)
+CREATE TABLE san_pham (
+    san_pham_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    ten_san_pham VARCHAR(150) NOT NULL UNIQUE,
+    gia_san_pham DECIMAL(12, 2) NOT NULL CHECK (gia_san_pham > 0)
 );
 
-CREATE TABLE `Order` (
-    oID INT PRIMARY KEY AUTO_INCREMENT,
-    cID INT,
-    oDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    oTotalPrice DECIMAL(12, 2),
-    FOREIGN KEY (cID) REFERENCES Customer(cID)
+CREATE TABLE don_hang (
+    don_hang_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    khach_hang_id INT UNSIGNED,
+    ngay_dat_hang DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    tong_tien DECIMAL(14, 2) CHECK (tong_tien >= 0),
+    FOREIGN KEY (khach_hang_id) REFERENCES khach_hang(khach_hang_id)
 );
 
-CREATE TABLE OrderDetail (
-    oID INT,
-    pID INT,
-    odQTY INT NOT NULL CHECK (odQTY > 0),
-    PRIMARY KEY (oID, pID),
-    FOREIGN KEY (oID) REFERENCES `Order`(oID),
-    FOREIGN KEY (pID) REFERENCES Product(pID)
+CREATE TABLE chi_tiet_don_hang (
+    don_hang_id INT UNSIGNED,
+    san_pham_id INT UNSIGNED,
+    so_luong SMALLINT UNSIGNED NOT NULL CHECK (so_luong > 0),
+    PRIMARY KEY (don_hang_id, san_pham_id),
+    FOREIGN KEY (don_hang_id) REFERENCES don_hang(don_hang_id),
+    FOREIGN KEY (san_pham_id) REFERENCES san_pham(san_pham_id)
 );
