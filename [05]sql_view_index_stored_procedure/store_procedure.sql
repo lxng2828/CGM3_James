4e33e80b402e68d7dd0f0c61b202a8e859a377f4
lxng <lxng@lxng>
2025-05-25 22:42:43 +0700

    .

---
+++ [05]sql_view_index_stored_procedure/store_procedure.sql
@@ -0,0 +1,25 @@
+USE classicmodels;
+
+DELIMITER //
+
+CREATE PROCEDURE findAllCustomers()
+BEGIN
+  SELECT * FROM customers;
+END //
+
+DELIMITER ;
+
+CALL findAllCustomers();
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS `findAllCustomers`//
+
+CREATE PROCEDURE findAllCustomers()
+BEGIN
+  SELECT * FROM customers WHERE customerNumber = 175;
+END //
+
+DELIMITER ;
+
+CALL findAllCustomers();
